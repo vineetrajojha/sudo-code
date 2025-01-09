7e4ada0ec2df652b7fa96f7ecd50232722b57532
Vineet Raj <vineetcres@gmail.com>
2025-01-10 00:17:14 +0530

    fibo

--- fiborecursion.java
+++ fiborecursion.java
@@ -1,15 +1,15 @@
-public class Fibonacci {
+public class fiborecursion {
     public static void main(String[] args) {
         int x = 10; 
+        System.out.println(x);
     }
 
-    
     public static int f(int x) {
         // Base
         if (x == 0) return 0; 
         if (x == 1) return 1; 
 
-        // Recursive relation: F(n) = F(n-1) + F(n-2)
+        // Recursion: F(n) = F(n-1) + F(n-2)
         return f(x - 1) + f(x - 2);
     }
 }
